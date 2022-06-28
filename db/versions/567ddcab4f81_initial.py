b5b0c910afdc10542da773089828d80ec81def7b
matthijsb <matthijs@dynora.nl>
2022-06-28 22:25:39 +0200

    - remove sqllite specific test workaround

--- db/versions/567ddcab4f81_initial.py
+++ db/versions/567ddcab4f81_initial.py
@@ -9,51 +9,13 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
-import app
+import apps
 
 # revision identifiers, used by Alembic.
 revision = '567ddcab4f81'
 down_revision = None
 branch_labels = None
 depends_on = None
-
-
-from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.dialects.mysql import MEDIUMINT, INTEGER, TEXT, TINYINT
-
-@compiles(TINYINT, 'sqlite')
-def compile_TINYINT(element, compiler, **kw):
-    """ Handles mysql TINYINT datatype as Integer in sqlite.  """
-    return compiler.visit_integer(element, **kw)
-
-@compiles(MEDIUMINT, 'sqlite')
-def compile_MEDIUMINT(element, compiler, **kw):
-    """ Handles mysql MEDIUMINT datatype as Integer in sqlite.  """
-    return compiler.visit_integer(element, **kw)
-
-@compiles(INTEGER, 'sqlite')
-def compile_INTEGER(element, compiler, **kw):
-    """ Handles mysql INTEGER datatype as Integer in sqlite.  """
-    return compiler.visit_integer(element, **kw)
-
-@compiles(TEXT, 'sqlite')
-def compile_TEXT(element, compiler, **kw):
-    """ Handles mysql TEXT datatype as text in sqlite.  """
-    return compiler.visit_text(element, **kw)
-
-@compiles(TEXT, 'sqlite')
-def compile_HASH(element, compiler, **kw):
-    """ Handles mysql BIN datatype as text in sqlite.  """
-    return compiler.visit_binary(element, **kw)
-
-@compiles(TEXT, 'sqlite')
-def compile_VARHASH(element, compiler, **kw):
-    """ Handles mysql VARBIN datatype as text in sqlite.  """
-    return compiler.visit_VARBINARY(element, **kw)
-
-
-
-
 
 
 def upgrade():
