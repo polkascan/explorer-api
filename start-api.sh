01df35a2487caffbb93e7ae8c0544307eb642a46
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2022-07-12 12:57:19 +0200

    Added DB check and alembic migrations

--- start-api.sh
+++ start-api.sh
@@ -1,4 +1,6 @@
 # If there's a prestart.sh script in the /app directory, run it before starting
 source ./prestart.sh
+# Execute DB migrations
+alembic upgrade head
 # Start Uvicorn with live reload
 exec uvicorn --host 0.0.0.0 --port 8000 --log-level info "app.main:app" --forwarded-allow-ips='*' --proxy-headers
